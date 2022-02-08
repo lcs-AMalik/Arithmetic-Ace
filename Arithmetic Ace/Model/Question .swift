5a35a88b2418d1a4bd88862505d76bf5aed3096e
Abdul <AMalik@lcs.on.ca>
2022-02-08 08:58:23 -0500

    Added Quiz view!

---
+++ Arithmetic Ace/Model/Question .swift
@@ -0,0 +1,39 @@
+//
+//  Question .swift
+//  Arithmetic Ace
+//
+//  Created by Abdul Malik on 2022-02-08.
+//
+
+import Foundation
+
+struct Quiz {
+    
+    let answer: String
+    let question: String
+    
+}
+
+let listOfQuiz = [
+
+    Quiz(answer: "5", question: "5 x 1")
+    ,
+    Quiz(answer: "10", question: "5 x 2")
+    ,
+    Quiz(answer: "15", question: "5 x 3")
+    ,
+    Quiz(answer: "20", question: "5 x 4")
+    ,
+    Quiz(answer: "25", question: "5 x 5")
+    ,
+    Quiz(answer: "30", question: "5 x 6")
+    ,
+    Quiz(answer: "35", question: "5 x 7")
+    ,
+    Quiz(answer: "40", question: "5 x 8")
+    ,
+    Quiz(answer: "45", question: "5 x 9")
+    ,
+    Quiz(answer: "50", question: "5 x 10")
+    
+]
